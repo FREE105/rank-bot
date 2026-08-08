fb7a5f4233447b4b2bd1e195f8694c1b1f9df66f
FREE105 <matusszell11@gmail.com>
2026-08-08 22:16:33 +0200

    index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages
+    ]
+});
+
+client.once('ready', () => {
+    console.log(`Bot je online ako ${client.user.tag}!`);
+});
+
+client.login(process.env.DISCORD_TOKEN);
